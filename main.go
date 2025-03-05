6aa314deb14b264d2b88f67d7f682b60e148b20c
Ilia Prikazchikov <ilyaprikaz@gmail.com>
2025-03-05 13:39:00 -0500

    feat: add admin endpoints'

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerResetMetrics)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetMetrics)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -38,8 +38,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(200)
-	res := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	res := fmt.Sprintf(`
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>
+`, cfg.fileserverHits.Load())
 	w.Write([]byte(res))
 }
